67323fcdb3ced50cf24ded030124ae9adacaf0cf
diongrendelman <info@complexpc.nl>
2021-04-29 15:51:18 +0200

    Buid main website.

---
+++ components/header/Header.js
@@ -0,0 +1,50 @@
+import styles from './Header.module.scss'
+import Head from "next/head";
+import {useState} from "react";
+
+const Header = (titleName) => {
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    const [toggleMobile, setMobile] = useState(false);
+
+    return (
+        <>
+            <Head>
+                <title>test</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <nav className={`${styles.header} navbar py-4 navbar-expand-lg navbar-dark sticky-top`}>
+                <div className="container justify-content-center">
+                    <button onClick={() => setMobile(!toggleMobile)} className="navbar-toggler" type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded={`${toggleMobile ? 'true' : 'false'}`} aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={`${!toggleMobile && 'collapse'} navbar-collapse`} id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center text-lg-start mt-4 mt-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#aboutme">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#projects">Projects</a>
+                            </li>
+                        </ul>
+                        <div className="navbar-text text-center text-lg-start">
+                            <a href=''>
+                                <i className="bi bi-github me-2"></i>
+                            </a>
+                        <a href=''>
+                                <i className="bi bi-linkedin"></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,131 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import Link from "next/link";
+import Image from "next/image";
+import Header from "../components/header/Header";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Header titleName={'Create Next App1'}/>
+            <div className={`${styles.hero}`}>
+                <div className={`container text-white`}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-5 text-center text-md-start'}>
+                            <div className='d-flex flex-column h-100 justify-content-center'>
+                                <h3 className={`fw-light `} style={{opacity: 0.75}}>Hi There!</h3>
+                                <h2 className={`text-primary fw-bold`}>I'm Dion Grendelman!</h2>
+                                <p className={`fw-lighter`}>A React / React Native developer.</p>
+                            </div>
+                        </div>
+                        <div className={'col-12 col-md-7'}>
+                            <img className='w-100 px-5 pt-5 pt-md-0' src="/images/BannerImage.svg" alt="Vercel Logo"
+                                 width={'100%'}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <div className={`${styles.aboutus}`} id={'aboutme'}>
+                <div className={`text-center text-white`}>
+                    <h2 className={`fw-bold`}>About me</h2>
+                    <p className='fw-lighter ' style={{opacity: 0.5}}>
+                        "Programming isn't about what you know; it's about what you can figure out"
+                    </p>
+                </div>
+                <div className={'container mt-5'}>
+                    <div className='row text-white justify-content-center'>
+                        <div className='col-lg-4'>
+                            <div className={'d-flex flex-column'}>
+                                <div className={`bg-secondary mb-2 p-4`}>
+                                    <h4>React</h4>
+                                    <div className={'fw-lighter'}>
+                                        The past 3 years i've worked with react to build the backend CMS for multiple
+                                        SAAS apps. I've worked with Typescript, NextJS, Redux, Electron and many other libraries.
+                                    </div>
+                                </div>
+                                <div className={`bg-secondary mb-2 p-4`}>
+                                    <h4>React Native</h4>
+                                    <div className={'fw-lighter'}>Building mobile apps with React Native is such a
+                                        awesome experience. I've made simple multiple "business card apps" and also some
+                                        Headless SAAS apps. I've worked with many libraries in the React Native
+                                        community.
+                                    </div>
+                                </div>
+                                <div className={`mb-2 p-4`} style={{backgroundColor:'rgba(50, 5, 142,0.3)'}}>
+                                    <h4>Many more...</h4>
+                                    <div className={'fw-lighter'}>My experience goes pretty wide. For example wordpress development, UX Design, VueJS, Symfony, Bootstrap, Electron, Java but my main focus is on React/React Native</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={'col-lg-4  offset-lg-1 mt-5 mt-lg-0'}>
+                            <div className='d-flex flex-column h-100 justify-content-center'>
+                                <div>
+                                    <h2 className={'fw-bold'}>
+                                        <span className={'fw-light'}>Hello!</span><br/>
+                                        I'm Dion Grendelman!
+                                    </h2>
+                                    <p className={'fw-lighter'}>
+                                        The last years i've been builing up my coding experience. I started as a wordpress developer (custom plugins/themes) and the last 2/3 years my focus has gone to "react".
+                                        My passion is in the front-end development. From a simple website to a full webapp or mobile app. Besides being a developer i also own a medium-large IT Company.
+                                        My best skill would be communication. Contact with customers is one of my favorite things to do.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <div className={`${styles.projects}`} id={'projects'}>
+                <div className={'text-white container'}>
+                    <div className={'row'}>
+                        <div className={`col-md-6 `}>
+                            <div className={'py-5'}>
+                                <h2>My Projects</h2>
+                                <p className={'fw-lighter'}>Lorazepam may increase the risk of serious or life</p>
+                                <button type="button" className="btn btn-primary text-white"><i
+                                    className="bi bi-collection me-2"></i> All projects
+                                </button>
+                            </div>
+                            <div className={`${styles.project}`}>
+                                <div className='p-4'>
+                                    <img style={{borderRadius: 15}} src="/images/placeholder.png"
+                                         alt="Vercel Logo"
+                                         width={'100%'}/></div>
+                            </div>
+                        </div>
+                        <div className={`col-md-6 `}>
+                            <div className={`${styles.project}`}>
+                                <div className='p-4'>
+                                    <img style={{borderRadius: 15}} src="/images/placeholder.png"
+                                         alt="Vercel Logo"
+                                         width={'100%'}/></div>
+                            </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+                            <div className={`${styles.project} mt-4`}>
+                                <div className='p-4'>
+                                    <img style={{borderRadius: 15}} src="/images/placeholder.png"
+                                         alt="Vercel Logo"
+                                         width={'100%'}/></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'text-center'} style={{marginTop: 100}}>
+                        <h4>Find me on</h4>
+                        <p className={`fw-lighter`}>Feel free to connect with me on</p>
+                        <div>
+                            <a href=''>
+                                <i className="bi bi-github me-2"></i>
+                            </a>
+                            <a href=''>
+                                <i className="bi bi-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
